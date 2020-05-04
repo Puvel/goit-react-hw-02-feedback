fc2fcdb1d999aa46731f54f0671fcf25a7f8d773
Puvel <puvelko@gmail.com>
2020-05-04 17:55:46 +0300

    Updates

---
+++ precache-manifest.2a7c719d2a1d24129370668e7c9c2130.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bb7405d7de3017f3f0a77b9b040cc9",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "d2288f8d7992fec95269",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3f4fbcb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3f4fbcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90c44310e614946fa45",
+    "url": "/goit-react-hw-02-feedback/static/js/main.375c23ff.chunk.js"
+  },
+  {
+    "revision": "03a203c26405944b1cc5",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d3f91252.js"
+  }
+]);
